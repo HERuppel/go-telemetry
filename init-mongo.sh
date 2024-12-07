47fef60b41b0175df82a3723508eec94b0d0eb2e
Renato Ruppel <heruppel2@gmail.com>
2024-12-06 22:31:37 -0300

    refact(mongo): add authentication

--- init-mongo.sh
+++ init-mongo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mongo <<EOF
+mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD <<EOF
 use telemetry;                
 db.createCollection("events")
 print("Database successfully created!");
