121850253ed06b5ae3afce98a61cdbc5c8e6c6fb
Renato Ruppel <heruppel2@gmail.com>
2024-12-08 11:51:33 -0300

    refact(scripts, mongo): create collection indexes at mongo container entrypoint

--- init-mongo.sh
+++ init-mongo.sh
@@ -3,6 +3,8 @@
 mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD <<EOF
 use $MONGO_DB_NAME;                
 db.createCollection($MONGO_DB_COLLECTION)
+db.$MONGO_DB_COLLECTION.createIndex({ "timestamp": 1 })
+db.$MONGO_DB_COLLECTION.createIndex({ "type": 1 })
 db.createCollection($MONGO_DB_METRICS_COLLECTION)
 print("Database successfully created!");
 EOF
