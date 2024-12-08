5b3b4dbe0b64b8c0e68842564c63475f23e769c1
Renato Ruppel <heruppel2@gmail.com>
2024-12-07 22:00:52 -0300

    refact(project): add metrics db collection env variable

--- init-mongo.sh
+++ init-mongo.sh
@@ -3,5 +3,6 @@
 mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD <<EOF
 use $MONGO_DB_NAME;                
 db.createCollection($MONGO_DB_COLLECTION)
+db.createCollection($MONGO_DB_METRICS_COLLECTION)
 print("Database successfully created!");
 EOF
